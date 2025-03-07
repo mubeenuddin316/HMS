3c35e7791d5366451ae646601ab868090c1627f0
Mohammed Mubeenuddin <mubeenuddin316@gmail.com>
2025-03-08 00:34:52 +0530

    link added after application is started

--- src/main/java/com/medorb/HMS/HospitalManagementSystemApplication.java
+++ src/main/java/com/medorb/HMS/HospitalManagementSystemApplication.java
@@ -3,13 +3,30 @@ package com.medorb.HMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 public class HospitalManagementSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HospitalManagementSystemApplication.class, args);
+        
+        //Run the application and store result in ctx
+        ConfigurableApplicationContext ctx = SpringApplication.run(HospitalManagementSystemApplication.class, args);
+		// 2) Grab the environment to find server port and context path
+
+		Environment env = ctx.getEnvironment();
+
+        // If not set, defaults to "8080" and "" (empty) for context path
+        String port = env.getProperty("server.port", "8080");
+        String contextPath = env.getProperty("server.servlet.context-path", "");
+
+        // 3) Print your desired link to the console
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Application is running! Access it here:");
+        System.out.println("http://localhost:" + port + contextPath + "/");
+        System.out.println("----------------------------------------------------------");
     }
 
     
